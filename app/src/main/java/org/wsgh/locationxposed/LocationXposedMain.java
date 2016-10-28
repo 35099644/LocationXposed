fb8d6884cc9e7e911de8a0fa7c9bbba157a9c0f3
zhangyipeng <sandy1108@163.com>
2016-10-28 19:08:49 +0800

    sync

--- app/src/main/java/org/wsgh/locationxposed/LocationXposedMain.java
+++ app/src/main/java/org/wsgh/locationxposed/LocationXposedMain.java
@@ -4,9 +4,18 @@ package org.wsgh.locationxposed;
  * Created by sandy on 2016/10/25.
  */
 
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class LocationXposedMain implements IXposedHookLoadPackage {
 
@@ -16,18 +25,46 @@ public class LocationXposedMain implements IXposedHookLoadPackage {
             return;
         }
         XposedBridge.log("Catch it, FUCK IT: " + lpparam.packageName);
-        findAndHookMethod("org.zywx.wbpalmstar.plugin.uexpunchzymobi.EUExPunchZymobi", lpparam.classLoader, "getLocation", new XC_MethodHook() {
+        XC_MethodHook hook = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                // this will be called before the clock was updated by the original method
             }
 
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                // this will be called after the clock was updated by the original method
-                Object obj = param.thisObject;
-                obj.
+            protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                Log.i("zypXposed","afterHookedMethod");
+                double latutude = 40.01547;
+                double longitude = 116.40761;
+                Object euexInstance = methodHookParam.thisObject;
+                Class euexClass = euexInstance.getClass();
+                Field latitudeField = euexClass.getDeclaredField("latitude");
+                latitudeField.setAccessible(true);
+                latitudeField.setDouble(euexInstance, latutude);
+                Field longitudeField = euexClass.getField("longitude");
+                longitudeField.setAccessible(true);
+                longitudeField.setDouble(euexInstance, longitude);
+                XposedBridge.log("Crack punch location Successfully!!! ");
             }
-        });
+        };
+        XC_MethodReplacement replacement = new XC_MethodReplacement(){
+
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable
+            {
+                double latutude = 40.01547;
+                double longitude = 116.40761;
+                Object euexInstance = methodHookParam.thisObject;
+                Class euexClass = euexInstance.getClass();
+                Field latitudeField = euexClass.getDeclaredField("latitude");
+                latitudeField.setAccessible(true);
+                latitudeField.setDouble(euexInstance, latutude);
+                Field longitudeField = euexClass.getField("longitude");
+                longitudeField.setAccessible(true);
+                longitudeField.setDouble(euexInstance, longitude);
+                XposedBridge.log("Crack punch location Successfully!!! ");
+                return null;
+            }
+        };
+        findAndHookMethod("org.zywx.wbpalmstar.plugin.uexpunchzymobi.EUExPunchZymobi", lpparam.classLoader, "getLocation", hook);
     }
 }
