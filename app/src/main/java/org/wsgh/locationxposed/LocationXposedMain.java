8e779b87e3efc79b45b34455a99ddcbdd3ad1bda
zhangyipeng <sandy1108@163.com>
2016-11-25 22:05:51 +0800

    完善开关

--- app/src/main/java/org/wsgh/locationxposed/LocationXposedMain.java
+++ app/src/main/java/org/wsgh/locationxposed/LocationXposedMain.java
@@ -11,6 +11,7 @@ import java.lang.reflect.Field;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -32,6 +33,14 @@ public class LocationXposedMain implements IXposedHookLoadPackage {
             @Override
             protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
                 Log.i("zypXposed","afterHookedMethod");
+                boolean isNeedHook = false;
+                XSharedPreferences sp = new XSharedPreferences(this.getClass()
+                        .getPackage().getName(), "pref_general");
+                isNeedHook = sp.getBoolean("globalSwitch",false);
+                if(!isNeedHook){
+                    Log.i("zypXposed","isNeedHook? "+isNeedHook);
+                    return;
+                }
                 double latitude = Utils.getRandom(40.014134, 40.016492);
                 double longitude = Utils.getRandom(116.40554, 116.409205);
                 Object euexInstance = methodHookParam.thisObject;
@@ -45,25 +54,25 @@ public class LocationXposedMain implements IXposedHookLoadPackage {
                 XposedBridge.log("Crack punch location Successfully!!! ");
             }
         };
-        XC_MethodReplacement replacement = new XC_MethodReplacement(){
-
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable
-            {
-                double latitude = 40.01547;
-                double longitude = 116.40761;
-                Object euexInstance = methodHookParam.thisObject;
-                Class euexClass = euexInstance.getClass();
-                Field latitudeField = euexClass.getDeclaredField("latitude");
-                latitudeField.setAccessible(true);
-                latitudeField.setDouble(euexInstance, latitude);
-                Field longitudeField = euexClass.getDeclaredField("longitude");
-                longitudeField.setAccessible(true);
-                longitudeField.setDouble(euexInstance, longitude);
-                XposedBridge.log("Crack punch location Successfully!!! ");
-                return null;
-            }
-        };
         findAndHookMethod("org.zywx.wbpalmstar.plugin.uexpunchzymobi.EUExPunchZymobi", lpparam.classLoader, "getLocation", hook);
+//        XC_MethodReplacement replacement = new XC_MethodReplacement(){
+//
+//            @Override
+//            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable
+//            {
+//                double latitude = 40.01547;
+//                double longitude = 116.40761;
+//                Object euexInstance = methodHookParam.thisObject;
+//                Class euexClass = euexInstance.getClass();
+//                Field latitudeField = euexClass.getDeclaredField("latitude");
+//                latitudeField.setAccessible(true);
+//                latitudeField.setDouble(euexInstance, latitude);
+//                Field longitudeField = euexClass.getDeclaredField("longitude");
+//                longitudeField.setAccessible(true);
+//                longitudeField.setDouble(euexInstance, longitude);
+//                XposedBridge.log("Crack punch location Successfully!!! ");
+//                return null;
+//            }
+//        };
     }
 }
